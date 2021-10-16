32414b5ea001903fc16ec8f62f2aaa393ad3a2ae
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-10-16 12:19:41 -0300

    rollback to brings back the real time hotwire functionality working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,19 @@
-// Import and register all your controllers from the importmap under controllers/*
+// this is commented because isnt working
 
+// import { application } from "controllers/application"
+// import { registerControllersFrom } from "@hotwired/stimulus-importmap-autoloader"
+// import StimulusReflex from 'stimulus_reflex'
+// import consumer from '../channels/consumer'
+// import controller from '../controllers/application_controller'
+// registerControllersFrom("controllers", application)
+// StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+// StimulusReflex.debug = process.env.RAILS_ENV === 'development'
+
+
+
+
+
+// code this works....
 import { application } from "controllers/application"
 import { registerControllersFrom } from "@hotwired/stimulus-importmap-autoloader"
-import StimulusReflex from 'stimulus_reflex'
-import consumer from '../channels/consumer'
-import controller from '../controllers/application_controller'
 registerControllersFrom("controllers", application)
-StimulusReflex.initialize(application, { consumer, controller, isolate: true })
-StimulusReflex.debug = process.env.RAILS_ENV === 'development'
